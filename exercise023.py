16af0acab557425fbd82ef9b94e94f75a88eb0ea
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-11 21:22:37 -0300

    add file exercise023.py

---
+++ exercise023.py
@@ -0,0 +1,7 @@
+num = input('Informe um número: ')
+
+print('Analisando o número {}'.format(num))
+print('Unidades: {}'.format(num[len(num)-1]))
+print('Dezenas: {}'.format(num[len(num)-2]))
+print('Centenas: {}'.format(num[len(num)-3]))
+print('Milhares: {}'.format(num[len(num)-4]))
