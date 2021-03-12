bd1d525a36b404f3f57a17e5bdc1c92c9c2bb0c8
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-12 13:38:48 -0300

    add file exercise038.py

---
+++ exercise038.py
@@ -0,0 +1,9 @@
+num1 = int(input('Insira um número I: '))
+num2 = int(input('Insira um número II: '))
+
+if num1 > num2:
+  print('O primeiro número é maior.')
+elif num1 < num2:
+  print('O segundo número é maior.')
+else:
+  print('Os números são iguais.')
