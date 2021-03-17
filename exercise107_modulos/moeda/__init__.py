21043d48e23e6dfa26934dd2da1d7a215663bfc1
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-16 23:59:59 -0300

    add file exercise110.py

--- exercise107_modulos/moeda/__init__.py
+++ exercise107_modulos/moeda/__init__.py
@@ -25,3 +25,13 @@ def porcentagem(preco, porcentagem, show_cifrao):
 def cifrao(numero):
   return f'R$ {numero:.2f}'
 
+
+def resumo(numero, aumento, reducao):
+  print('-'*40)
+  print('Resumo do valor'.center(40))
+  print('-'*40)
+
+  print(f'Preço analisado: {cifrao(numero)}')
+  print(f'Dobro do preço: {dobro(numero, True)}')
+  print(f'{aumento}% do preço: {porcentagem(numero,aumento, True)}')
+  print(f'{reducao}% do preço: {porcentagem(numero,aumento, True)}')
---
+++ exercise110.py
@@ -0,0 +1,4 @@
+from exercise107_modulos import moeda
+
+preco = float(input('Digite o preço: '))
+moeda.resumo(preco,80,20)
